2955efc1f043fb862d91f389ed48064ddb8db8f6
artemave <artemave@gmail.com>
2018-09-23 22:10:14 +0200

    Move shared code into a shared location
    For less duplication.

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ const argv = require('yargs')
 const appDir = path.resolve(process.cwd(), argv._[0])
 fs.ensureDirSync(appDir)
 
+const sharedTemplateDir = path.resolve(__dirname, 'template', 'shared')
+fs.copySync(sharedTemplateDir, appDir)
+
 let templateDir
 if (argv.jsx) {
   templateDir = path.resolve(__dirname, 'template', 'js', 'jsx')
 } else {
   templateDir = path.resolve(__dirname, 'template', 'js', 'js')
 }
+
 fs.copySync(templateDir, appDir)
 
 console.info('Done!')
--- template/js/jsx/scripts/start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$NODE_ENV" = 'production' ]; then
-  node ./server/app.js
-else
-  nodemon --ignore **/dist ./server/app.js
-fi
